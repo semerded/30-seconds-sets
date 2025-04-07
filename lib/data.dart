4717e7bffed228869b8513604728ecbeaa426128
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-04-07 23:26:42 +0200

    update colors for better visibility

--- lib/data.dart
+++ lib/data.dart
@@ -7,4 +7,8 @@ class Palette {
   static const Color secondary = Colors.deepOrange;
   static const Color bgLight = Colors.white;
   static const Color bgDark = Colors.black;
+  static const Color buttonLight = Color.fromARGB(255, 222, 222, 222);
+  static const Color buttonDark = Color.fromARGB(255, 72, 72, 72);
+  static const Color textLight = Colors.white;
+  static const Color textDark = Colors.black;
 }
